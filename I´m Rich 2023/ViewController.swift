a9fd57cd267b4acf54a9e7d27e2e776819531e76
Fabiana Limma <fabianallima@hotmail.com>
2023-04-23 19:09:01 -0300

    first commit

---
+++ I´m Rich 2023/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I´m Rich 2023
+//
+//  Created by Fabiana Limma on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    
+    }
+
+
+}
